4aba84d49480103127c4fc47f62d23c18be0c7db
ivan schlagheck <ivansch05@gmail.com>
2022-01-13 11:41:30 +0100

    finishing module

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,65 +1,93 @@
-import React, {useState} from "react";
+import React, { useState, useRef } from "react";
+
 import Card from "../UI/Card";
-import Button from "../UI/Button"
+import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
-import styles from './AddUser.module.css'
+import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  //initialite useRef
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   // Initialize useState
-  const [enteredUserName, setEnteredUserName] = useState('')
-  const [enteredAge, setEnteredAge] = useState('')
-  const [error, setError] = useState()
+  // const [enteredUserName, setEnteredUserName] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   // Adds User
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
-        title: 'Inavalid input',
-        message: 'Please enter a valid name and age (non-empty-values)'
+        title: "Inavalid input",
+        message: "Please enter a valid name and age (non-empty-values)",
       });
-      return
+      return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
-        title: 'Inavalid age',
-        message: 'Please enter a valid age (age > 0)'
+        title: "Inavalid age",
+        message: "Please enter a valid age (age > 0)",
       });
-      return
+      return;
     }
-    props.onAddUser(enteredUserName, enteredAge);
-    setEnteredUserName('');
-    setEnteredAge('');
+    props.onAddUser(enteredName, enteredUserAge);
+    // setEnteredUserName("");
+    // setEnteredAge("");
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   //Saves Name and Age from inputs
-  const userNameChangeHandler = (event)=> {
-    setEnteredUserName(event.target.value);
-  }
-  const ageChangeHandler = (event)=> {
-    setEnteredAge(event.target.value);
-  }
+  // const userNameChangeHandler = (event) => {
+  //   setEnteredUserName(event.target.value);
+  // };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(null);
   };
-  
+
   return (
-    <div>
-      {error && <ErrorModal title={error.title} message= {error.message} onConfirm= {errorHandler} />}
-      <Card className={styles.input} >
-        <form onSubmit = {addUserHandler}>
+    <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" value={enteredUserName} onChange ={userNameChangeHandler}/>
+          <input
+            type="text"
+            id="username"
+            // value={enteredUserName}
+            // onChange={userNameChangeHandler}
+            ref={nameInputRef}
+          />
           <label htmlFor="age">Age (Years)</label>
-          <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler}/>
-      
-          <Button type= 'submit'>Add User</Button>
+          <input
+            type="number"
+            id="age"
+            // value={enteredAge}
+            // onChange={ageChangeHandler}
+            ref={ageInputRef}
+          />
+
+          <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
-  )
+    </>
+  );
 };
 
 export default AddUser;
